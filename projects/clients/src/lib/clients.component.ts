d67e425dc9687fb864b507b5cb08322d7584e95c
Karen Rincón <karenrinconjm2016@gmail.com>
2025-01-29 10:10:47 -0500

    chore: Initial commit with project structure

---
+++ projects/clients/src/lib/clients.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-clients',
+  imports: [],
+  template: `
+    <p>
+      clients works!
+    </p>
+  `,
+  styles: ``
+})
+export class ClientsComponent {
+
+}
