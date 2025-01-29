fd30420af66d263cbcb50ca7f9e35836f73fb1f0
Hernan-Alonso-Calderon <hernanalonsocalderon@gmail.com>
2025-01-29 00:27:44 -0500

    chore: initial project structure

--- projects/app/src/app/app.routes.ts
+++ projects/app/src/app/app.routes.ts
@@ -3,9 +3,9 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'users',
-    loadChildren: () => import('users').then(m => m.usersRoutes)
+    loadChildren: () => import('users').then((m) => m.usersRoutes),
   },
-  // {
+  //{
   //   path: '',
   //   loadChildren: () => import('home').then(m => m.homeRoutes)
   // }
