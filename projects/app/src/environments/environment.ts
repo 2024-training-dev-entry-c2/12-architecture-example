f39ffd7ec21e86d0ac0c211872a8b6a47f64217d
Bymarium <bymarium@gmail.com>
2025-01-29 13:44:14 -0500

    create environments for urls

---
+++ projects/app/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrls: {
+    client: 'http://localhost:8080/api/clients',
+    menu: 'http://localhost:8080/api/menus',
+    dish: 'http://localhost:8080/api/dishes',
+    order: 'http://localhost:8080/api/orders',
+  }
+};
