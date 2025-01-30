6d06621f285eb20f49b86e2daa5aa76d7249aee4
Bymarium <bymarium@gmail.com>
2025-01-29 20:07:08 -0500

    create reusable components like input, select, modal, confirm-action and button

--- projects/shared/src/infrastructure/ui/components/table/table.component.ts
+++ projects/shared/src/infrastructure/ui/components/table/table.component.ts
@@ -1,10 +1,13 @@
 import { NgFor } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { IColumn } from '../../../../domain/model/column.model';
+import { ButtonComponent } from '../button/button.component';
+import { ModalComponent } from '../modal/modal.component';
+import { ConfirmActionComponent } from '../confirm-action/confirm-action.component';
 
 @Component({
   selector: 'lib-table',
-  imports: [NgFor],
+  imports: [NgFor, ButtonComponent, ModalComponent, ConfirmActionComponent],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
@@ -12,4 +15,37 @@ export class TableComponent {
   public title = input<string>();
   public data = input<any[]>();
   public columns = input<IColumn[]>();
+  public idDelete = output<number>();
+  public idUpdate = output<number>();
+  public openModal = output<boolean>();
+  public openDetailsModal = output<number>();
+  public selectedId: number = 0;
+  isOpen: boolean = false;
+
+  openConfirmation(id: number) {
+    this.selectedId = id; 
+    this.isOpen = true;
+  }
+
+  confirmDelete() {
+    this.idDelete.emit(this.selectedId);
+    this.isOpen = false;
+  }
+
+  cancelDelete() {
+    this.openModal.emit(false); 
+    this.isOpen = false;
+  }
+
+  sendUpdate(number: number) {
+    this.idUpdate.emit(number);
+  }
+
+  public openForm() {
+    this.openModal.emit(true);
+  }                                                       
+
+  public openDetails(id: number) {
+    this.openDetailsModal.emit(id);
+  }
 }
--- projects/shared/src/public-api.ts
+++ projects/shared/src/public-api.ts
@@ -1,4 +1,8 @@
 export { MainLayoutComponent } from './infrastructure/ui/layouts/main-layout/main-layout.component';
 export { BodyComponent } from './infrastructure/ui/components/body/body.component';
 export { TableComponent } from './infrastructure/ui/components/table/table.component';
+export { ModalComponent } from './infrastructure/ui/components/modal/modal.component';
+export { InputComponent } from './infrastructure/ui/components/input/input.component';
+export { SelectComponent } from './infrastructure/ui/components/select/select.component';
+export type { IResponse } from './domain/model/response.model';
 export { environment } from './infrastructure/ui/environments/environment';
