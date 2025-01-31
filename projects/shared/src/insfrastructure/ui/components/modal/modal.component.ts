bee6d232da59c3a283fe81153ee7551842ecac9c
Johan <johanvilla64@gmail.com>
2025-01-31 15:50:10 -0500

    feat(shared): :sparkles: add ModalComponent with HTML, SCSS

---
+++ projects/shared/src/insfrastructure/ui/components/modal/modal.component.ts
@@ -0,0 +1,19 @@
+import { Component, input } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'lib-modal',
+  imports: [ButtonComponent, NgClass],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss',
+})
+export class ModalComponent {
+  public action = input.required<string>();
+  public visible = false;
+
+  toggle() {
+    console.log('activacion del modal!!');
+    this.visible = !this.visible;
+  }
+}
