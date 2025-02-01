b45656f75f4a820e38045b9bb5005f2752ab491d
Johan <johanvilla64@gmail.com>
2025-02-01 16:57:26 -0500

    feat(shared): :sparkles: add CustomerFormComponent with HTML, SCSS for crearting and editing customers

---
+++ projects/customer/src/infrastructure/ui/forms/customer-form/customer-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, Input, output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ICustomer } from '../../../../domain/models/customer.model';
+
+@Component({
+  selector: 'lib-customer-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './customer-form.component.html',
+  styleUrl: './customer-form.component.scss',
+})
+export class CustomerFormComponent {
+  private readonly _fb = inject(FormBuilder);
+  public onSubmit = output<ICustomer>();
+
+  @Input()
+  set customer(value: ICustomer) {
+    this.form.patchValue(value as any);
+  }
+
+  public form = this._fb.group({
+    name: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required],
+    phone: ['', Validators.required],
+    orderIds: [[]],
+    // --------------------------------
+    customerId: [null],
+  });
+
+  submit() {
+    if (!this.form.valid) return;
+    this.onSubmit.emit(this.form.getRawValue());
+  }
+}
