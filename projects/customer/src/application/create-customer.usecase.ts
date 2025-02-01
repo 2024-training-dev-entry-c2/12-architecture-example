e074af3dbd6ad3b09c0d1f9bbeb87861592f4df8
Johan <johanvilla64@gmail.com>
2025-02-01 18:05:33 -0500

    feat(shared): :sparkles: add CreateCustomerUseCase for handling customer creation logic and state management

---
+++ projects/customer/src/application/create-customer.usecase.ts
@@ -0,0 +1,42 @@
+import { inject, Injectable } from '@angular/core';
+import { CreateCustomerService } from '../infrastructure/services/create-customer.service';
+import { State } from '../domain/state';
+import { Subscription, tap } from 'rxjs';
+import { ModalComponent } from 'shared';
+import { ICustomerCreateRequest } from '../domain/models/customer.model';
+import { CustomersState } from '../domain/state/customers.state';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CreateCustomerUseCase {
+  private readonly _service = inject(CreateCustomerService);
+  private readonly _state = inject(State);
+  private subscriptions: Subscription;
+
+  //   #region Public methods
+  initSubscription() {
+    this.subscriptions = new Subscription();
+  }
+
+  destroySubscriptions() {
+    this.subscriptions.unsubscribe();
+  }
+
+  execute(customer: ICustomerCreateRequest, modal: ModalComponent): void {
+    this.initSubscription();
+    this.subscriptions.add(
+      this._service
+        .execute(customer)
+        .pipe(
+          tap((c) => {
+            const customers = this._state.customerState.customers.snapshot();
+            this._state.customerState.customers.set([...customers, c]);
+            modal.toggle();
+          })
+        )
+        .subscribe()
+    );
+  }
+  //#endregion
+}
