4697897ad1d80c85d745a564192dbcedc2085ab3
Bymarium <bymarium@gmail.com>
2025-02-01 10:50:58 -0500

    create test for the body component

--- projects/shared/src/infrastructure/ui/components/body/body.component.spec.ts
+++ projects/shared/src/infrastructure/ui/components/body/body.component.spec.ts
@@ -1,23 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { BodyComponent } from './body.component';
 
 describe('BodyComponent', () => {
-  let component: BodyComponent;
-  let fixture: ComponentFixture<BodyComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BodyComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(BodyComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    }).compileComponents();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(BodyComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it('should render the body', () => {
+    const fixture = TestBed.createComponent(BodyComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('body')).toBeTruthy();
+  });
+
+  it('should render the router-outlet', () => {
+    const fixture = TestBed.createComponent(BodyComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
