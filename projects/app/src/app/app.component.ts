8feecaf147ce011ddbc9325d3c37c364cd12990d
Nahuel Lemes <nahulem@gmail.com>
2025-02-01 10:42:11 -0300

    feat(project): :sparkles: create App project

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,16 +1,12 @@
-import { Component, input, output } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: '<router-outlet /> <p>{{title()}}</p> <button (click)="handleClick()">Click me</button>',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = input<string>();
-  onClick = output<void>();
-
-  handleClick() {
-    this.onClick.emit();
-  }
+  title = 'app';
 }
--- projects/app/src/app/app.routes.ts
+++ projects/app/src/app/app.routes.ts
@@ -1,12 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: 'users',
-    loadChildren: () => import('users').then(m => m.usersRoutes)
-  },
-  // {
-  //   path: '',
-  //   loadChildren: () => import('home').then(m => m.homeRoutes)
-  // }
-];
+export const routes: Routes = [];
