964032610186df0f2ef5130a6037828937217645
Johan <johanvilla64@gmail.com>
2025-02-01 18:35:14 -0500

    fix(shared): :bug: update resource ID types to use number for improved type safety and API coherence

--- projects/shared/src/environments/environment.ts
+++ projects/shared/src/environments/environment.ts
@@ -7,8 +7,8 @@ export const RESOURCES = {
   DISHES: `${ENV.BASE_URL}/dishes`,
   MENUS: `${ENV.BASE_URL}/menus`,
   ORDERS: `${ENV.BASE_URL}/orders`,
-  customerById: (id: string) => `${RESOURCES.CUSTOMERS}/${id}`,
-  dishById: (id: string) => `${RESOURCES.DISHES}/${id}`,
-  menuById: (id: string) => `${RESOURCES.MENUS}/${id}`,
-  orderById: (id: string) => `${RESOURCES.ORDERS}/${id}`,
+  customerById: (id: number) => `${RESOURCES.CUSTOMERS}/${id}`,
+  dishById: (id: number) => `${RESOURCES.DISHES}/${id}`,
+  menuById: (id: number) => `${RESOURCES.MENUS}/${id}`,
+  orderById: (id: number) => `${RESOURCES.ORDERS}/${id}`,
 };
