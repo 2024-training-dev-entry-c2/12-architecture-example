40f88653101e7a7e5c40bd3e4003c301fab28ba7
danielaser <daniela.serrano.dev@gmail.com>
2025-01-31 23:16:53 -0500

    feat(develop): add inputs to client form

--- projects/client/src/infrastructure/ui/forms/client-form/client-form.component.ts
+++ projects/client/src/infrastructure/ui/forms/client-form/client-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, Input } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IClient } from '../../../../domain/model/clients.model';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'lib-client-form',
@@ -14,6 +14,12 @@ private readonly fb = inject(FormBuilder);
 
   @Input() isEditForm: boolean = false;
   @Input() currentClientName: string = '';
+  @Input() currentEmail: string = '';
+  @Input() currentPhoneNumber: string = '';
+  @Input() currentAddress: string = '';
+  @Input() currentRegistrationDate: Date = new Date();
+  @Input() currentFrequentUser: boolean = false;
+  @Input() currentVip: boolean = false;
 
   public form: FormGroup = this.fb.group({
     clientName: ['', Validators.required],
@@ -26,8 +32,16 @@ private readonly fb = inject(FormBuilder);
   });
 
   ngOnChanges(): void {
-    if (this.isEditForm && this.currentClientName) {
-      this.form.patchValue({ clientName: this.currentClientName });
+    if (this.isEditForm && this.currentClientName ) {
+      this.form.patchValue(
+        { clientName: this.currentClientName,
+          email: this.currentEmail,
+          phoneNumber: this.currentPhoneNumber,
+          address: this.currentAddress,
+          registrationDate: this.currentRegistrationDate,
+          frequentUser: this.currentFrequentUser,
+          vip: this.currentVip
+        });
     }
   }
 
