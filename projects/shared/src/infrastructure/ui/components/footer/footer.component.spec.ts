dfa0e0ca2df6a750b694cecf81863d2e51fdb4b8
Bymarium <bymarium@gmail.com>
2025-02-01 11:46:17 -0500

    create test for footer component

--- projects/shared/src/infrastructure/ui/components/footer/footer.component.spec.ts
+++ projects/shared/src/infrastructure/ui/components/footer/footer.component.spec.ts
@@ -3,21 +3,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [FooterComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(FooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    }).compileComponents();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it('should render the footer', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('footer')).toBeTruthy();
+  });
+
+  it('should render the copyright', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('p')).toBeTruthy();
+    expect(compiled.querySelector('p').textContent).toBe('© 2025 Reino de las Hadas. Todos los derechos reservados.');
   });
 });
