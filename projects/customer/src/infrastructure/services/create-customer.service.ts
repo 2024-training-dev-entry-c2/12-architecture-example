3e80dd21d5e193a97202a541e6d8a5941335e0bc
Johan <johanvilla64@gmail.com>
2025-02-01 17:51:10 -0500

    feat(shared): :sparkles: enhance CreateCustomerService to execute customer creation with ICustomerCreateRequest

--- projects/customer/src/infrastructure/services/create-customer.service.ts
+++ projects/customer/src/infrastructure/services/create-customer.service.ts
@@ -1,11 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import {
+  ICustomer,
+  ICustomerCreateRequest,
+} from '../../domain/models/customer.model';
+import { RESOURCES } from 'shared';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CreateCustomerService {
-  private http = inject(HttpClient);
+  private _http = inject(HttpClient);
 
-  execute() {}
+  execute(customer: ICustomerCreateRequest) {
+    return this._http.post<ICustomer>(RESOURCES.CUSTOMERS, customer);
+  }
 }
