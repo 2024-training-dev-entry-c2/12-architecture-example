ea74138f743b4a1d98441aaf71db204b73f27e6f
Johan <johanvilla64@gmail.com>
2025-02-02 13:58:23 -0500

    feat(routes): add routing for DishesContainerComponent

---
+++ projects/dish/src/infrastructure/ui/routes/dish.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { DishesContainerComponent } from '../containers/dishes-container/dishes-container.component';
+
+export const dishRoutes: Routes = [
+  {
+    path: '',
+    component: DishesContainerComponent,
+  },
+];
