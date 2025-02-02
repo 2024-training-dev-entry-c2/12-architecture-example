3a2686c3c9526c5b370ab2e03a455ffc21315272
Marty Parada <martin.p1131@gmail.com>
2025-02-02 08:58:53 -0500

    feat(main): test to pagination component

--- projects/shared/src/infrastructure/ui/components/modal/modal.component.spec.ts
+++ projects/shared/src/infrastructure/ui/components/modal/modal.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModalComponent } from './modal.component';
+import { FormGroup } from '@angular/forms';
 
 describe('ModalComponent', () => {
   let component: ModalComponent;
@@ -14,10 +15,30 @@ describe('ModalComponent', () => {
 
     fixture = TestBed.createComponent(ModalComponent);
     component = fixture.componentInstance;
+    component.formGroup = new FormGroup({}); //inicializar con un FormGroup vacío
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('shoud  emit closeModal event when close is called', () => {
+    spyOn(component.closeModal, 'emit');
+    component.close();
+    expect(component.closeModal.emit).toHaveBeenCalled();
+  });
+  it('should generate form fields from data', () => {
+    const testData = {
+      name: 'John Doe',
+      email: 'john@example.com',
+      isActive: true,
+    };
+    component.data = testData;
+    component.ngOnInit();
+
+    expect(component.fields.length).toBe(3);
+    expect(component.fields[0].name).toBe('name');
+    expect(component.fields[1].name).toBe('email');
+
   });
 });
