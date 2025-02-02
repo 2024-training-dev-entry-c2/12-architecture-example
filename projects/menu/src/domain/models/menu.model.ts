9dbb0a4a306a4d8f2d54f692566ff2244039b9d1
Johan <johanvilla64@gmail.com>
2025-02-02 17:44:23 -0500

    feat(menu.model): :sparkles: define IDish interface and add IMenuResponse interface

--- projects/menu/src/domain/models/menu.model.ts
+++ projects/menu/src/domain/models/menu.model.ts
@@ -1,8 +1,20 @@
-import { IDish } from '../../../../dish/src/domain/model/dish.model';
+export interface IDish {
+  id: number;
+  name: string;
+  price: number;
+  menuId: number;
+}
 
 export interface IMenu {
   menuId: number;
   name: string;
   description: string;
   dishes: IDish[];
+}
+
+export interface IMenuResponse {
+  menuId: number;
+  name: string;
+  description: string;
+  dishesIds: number[];
 }
