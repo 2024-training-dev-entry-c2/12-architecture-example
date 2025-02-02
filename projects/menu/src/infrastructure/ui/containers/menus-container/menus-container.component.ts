bceb5015f24a225c68f00acbdc8745c337f1f169
Johan <johanvilla64@gmail.com>
2025-02-02 15:48:13 -0500

    feat(menus-container): :sparkles: add MenusContainerComponent for managing menu creation and updates

---
+++ projects/menu/src/infrastructure/ui/containers/menus-container/menus-container.component.ts
@@ -0,0 +1,54 @@
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ModalComponent } from 'shared';
+import { IMenu } from '../../../../domain/models/menu.model';
+import { ListMenusComponent } from '../../components/list-dishes/list-menus.component';
+import { CreateMenuUseCase } from '../../../../application/create-menu.usecase';
+import { GetMenusUseCase } from '../../../../application/get-menus.usecase';
+import { UpdateMenuUseCase } from '../../../../application/update-menu.usecase';
+
+@Component({
+  selector: 'lib-menus-container',
+  imports: [AsyncPipe, ListMenusComponent],
+  templateUrl: './menus-container.component.html',
+})
+export class MenusContainerComponent {
+  private readonly _getUseCase = inject(GetMenusUseCase);
+  private readonly _createUseCase = inject(CreateMenuUseCase);
+  private readonly _updateUseCase = inject(UpdateMenuUseCase);
+
+  public menus$: Observable<IMenu[]>;
+  public currentMenu$: Observable<IMenu>;
+
+  //#region lifeCycle methods
+  ngOnInit() {
+    this._getUseCase.initSubscriptions();
+    this._createUseCase.initSubscriptions();
+    this._updateUseCase.initSubscriptions();
+
+    this._getUseCase.execute();
+
+    this.menus$ = this._getUseCase.menus$();
+    this.currentMenu$ = this._updateUseCase.currentMenu$();
+  }
+
+  ngOnDestroy() {
+    this._getUseCase.destroySubscriptions();
+    this._createUseCase.destroySubscriptions();
+    this._updateUseCase.destroySubscriptions();
+  }
+  //#endregion
+
+  // #region public methods
+  handlePatchMenu({ menu, modal }: { menu: IMenu; modal: ModalComponent }) {
+    const usecase = menu.menuId ? this._updateUseCase : this._createUseCase;
+    usecase.execute(menu, modal);
+  }
+
+  handleSelectMenu(menuId: number) {
+    this._updateUseCase.selectMenu(menuId);
+  }
+
+  // #endregion
+}
