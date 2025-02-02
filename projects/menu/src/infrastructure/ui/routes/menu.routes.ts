5c2eac9041da67603f66006d51ddd5980db52bcc
Johan <johanvilla64@gmail.com>
2025-02-02 16:15:14 -0500

    refactor(routes): :recycle: rename dishRoutes to menuRoutes and update public API exports

--- projects/menu/src/infrastructure/ui/routes/menu.routes.ts
+++ projects/menu/src/infrastructure/ui/routes/menu.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { MenusContainerComponent } from '../containers/menus-container/menus-container.component';
 
-export const dishRoutes: Routes = [
+export const menuRoutes: Routes = [
   {
     path: '',
     component: MenusContainerComponent,
--- projects/menu/src/public-api.ts
+++ projects/menu/src/public-api.ts
@@ -1,1 +1,4 @@
-export { MenusContainerComponent } from './infrastructure/ui/containers/menus-container/menus-container.component';
+export { menuRoutes } from './infrastructure/ui/routes/menu.routes';
+export * from './domain/models/menu.model';
+export * from './infrastructure/ui/containers/menus-container/menus-container.component';
+export * from './infrastructure/ui/forms/menu-form/menu-form.component';
