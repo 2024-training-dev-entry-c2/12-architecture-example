48079a4b6c6f6dd7e6f7c08bcbfd043cacd9bd26
Marty Parada <martin.p1131@gmail.com>
2025-02-02 08:43:35 -0500

    feat(main):  test to mainLayout

--- projects/shared/src/infrastructure/ui/components/layouts/main-layout/main-layout.component.spec.ts
+++ projects/shared/src/infrastructure/ui/components/layouts/main-layout/main-layout.component.spec.ts
@@ -2,8 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainLayoutComponent } from './main-layout.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { HeaderNavbarComponent } from '../../header/header.component';
 
 describe('MainLayoutComponent', () => {
   let component: MainLayoutComponent;
@@ -11,7 +13,7 @@ describe('MainLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule,MainLayoutComponent], // Agregamos el módulo de testeo del router
+      imports: [RouterTestingModule, MainLayoutComponent], // Agregamos el módulo de testeo del router
     }).compileComponents();
 
     fixture = TestBed.createComponent(MainLayoutComponent);
@@ -21,5 +23,17 @@ describe('MainLayoutComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should to render header and router-outlet', () => {
+    const header = fixture.debugElement.query(
+      By.directive(HeaderNavbarComponent)
+    );
+    const routerOutlet = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(header).toBeTruthy();
+    expect(routerOutlet).toBeTruthy();
+  });
+  it('should to render HeaderNavbarComponent ', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('lib-header-navbar')).toBeTruthy();
   });
 });
