a101bd49ecbd0cdd4ed3123e4a77a6745a4eb37c
Bymarium <bymarium@gmail.com>
2025-02-02 13:36:46 -0500

    organize code

--- projects/client/src/infrastructure/ui/forms/client-form/client-form.component.ts
+++ projects/client/src/infrastructure/ui/forms/client-form/client-form.component.ts
@@ -17,11 +17,6 @@ export class ClientFormComponent implements OnInit {
   public open$ = input<Observable<boolean>>();
   public onSubmit = output<IClient>();
 
-  @Input()
-  set client(value: IClient) {
-    this.form.patchValue(value);
-  }
-
   public form: FormGroup = this.formBuilder.group({
     id: [null],
     name: ['', [Validators.required]],
@@ -34,6 +29,11 @@ export class ClientFormComponent implements OnInit {
     { type: 'input', text: 'Apellido', inputType: 'text', controlName: 'lastName' },
     { type: 'input', text: 'Correo', inputType: 'email', controlName: 'email' }
   ];
+
+  @Input()
+  set client(value: IClient) {
+    this.form.patchValue(value);
+  }
 
   ngOnInit() {
     this.open$().subscribe(result => {
