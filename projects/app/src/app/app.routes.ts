752363048c5c5f6b0adbdaf80fee552a31ff5cc7
Johan <johanvilla64@gmail.com>
2025-02-02 13:58:43 -0500

    feat(routes): add routing for dishes to improve modularity

--- projects/app/src/app/app.routes.ts
+++ projects/app/src/app/app.routes.ts
@@ -15,6 +15,10 @@ export const routes: Routes = [
         path: 'customers',
         loadChildren: () => import('customer').then((m) => m.customerRoutes),
       },
+      {
+        path: 'dishes',
+        loadChildren: () => import('dish').then((m) => m.dishRoutes),
+      },
     ],
   },
 ];
