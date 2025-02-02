3d2b0ff1b08c4da95dc6a12acc0d88c58e3a8e2d
danielaser <daniela.serrano.dev@gmail.com>
2025-02-01 23:38:03 -0500

    feat(develop): add order header component

--- projects/order/src/infrastructure/ui/components/order-header/order-header.component.ts
+++ projects/order/src/infrastructure/ui/components/order-header/order-header.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { HeaderComponent } from "../../../../../../shared/src/infrastructure/ui/components/header/header.component";
+import { OrderFormComponent } from "../../forms/order-form/order-form.component";
+import { IOrder } from '../../../../domain/model/orders.model';
 
 @Component({
   selector: 'lib-order-header',
-  imports: [],
+  imports: [HeaderComponent, OrderFormComponent],
   templateUrl: './order-header.component.html',
   styleUrl: './order-header.component.scss'
 })
 export class OrderHeaderComponent {
+  @Output() onSubmitOrder = new EventEmitter<IOrder>();
+  @ViewChild(OrderFormComponent) menuForm!: OrderFormComponent;
 
+  submitOrder(): void {
+    // if (this.menuForm && this.menuForm.form.valid) {
+    //   const menu = this.menuForm.getFormData();
+    //   if (menu) {
+    //     console.log('Sending menu data:', menu);
+    //     this.onSubmitMenu.emit(menu);
+    //     this.menuForm.resetForm();
+    //   }
+    // }
+  }
 }
