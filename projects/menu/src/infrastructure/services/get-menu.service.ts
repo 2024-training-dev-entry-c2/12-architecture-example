acc7721f4bab13ae7984cc8cbc7f136d33f2e29c
Johan <johanvilla64@gmail.com>
2025-02-02 15:45:32 -0500

    feat(menu-service): :sparkles: add GetMenusService for fetching menu data via HTTP

---
+++ projects/menu/src/infrastructure/services/get-menu.service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IMenu } from '../../domain/models/menu.model';
+import { RESOURCES } from 'shared';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetMenusService {
+  private readonly _http = inject(HttpClient);
+
+  execute(): Observable<IMenu[]> {
+    return this._http.get<IMenu[]>(RESOURCES.CUSTOMERS);
+  }
+}
