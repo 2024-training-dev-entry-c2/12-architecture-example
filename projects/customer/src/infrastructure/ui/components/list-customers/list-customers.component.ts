f856ce7ae4badfcf2cd126ad8cfaf8d8423dfff1
Johan <johanvilla64@gmail.com>
2025-02-02 09:31:18 -0500

    feat(list-customers): :sparkles: add confirmation modal for customer deletion

--- projects/customer/src/infrastructure/ui/components/list-customers/list-customers.component.ts
+++ projects/customer/src/infrastructure/ui/components/list-customers/list-customers.component.ts
@@ -1,9 +1,7 @@
 import { NgFor } from '@angular/common';
 import { Component, input, output, viewChild } from '@angular/core';
 import { ModalComponent } from 'shared';
-import {
-  ICustomer
-} from '../../../../domain/models/customer.model';
+import { ICustomer } from '../../../../domain/models/customer.model';
 import { CustomerFormComponent } from '../../forms/customer-form/customer-form.component';
 
 @Component({
@@ -14,14 +12,20 @@ import { CustomerFormComponent } from '../../forms/customer-form/customer-form.c
 })
 export class ListCustomersComponent {
   public modal = viewChild<ModalComponent>('modal');
+  public deleteModal = viewChild<ModalComponent>('deleteModal');
   public customers = input.required<ICustomer[]>();
   public currentCustomer = input<ICustomer>();
+  public customerToDelete: ICustomer | null = null;
+
   public onCreateCustomer = output<{
     customer: ICustomer;
     modal: ModalComponent;
   }>();
   public onSelectCustomer = output<number>();
-  component: any;
+  public onDeleteCustomer = output<{
+    customerId: number;
+    modal: ModalComponent;
+  }>();
 
   handleSubmit(customer: ICustomer) {
     this.onCreateCustomer.emit({ customer, modal: this.modal() });
@@ -30,5 +34,25 @@ export class ListCustomersComponent {
   selectCustomer(id: number) {
     this.onSelectCustomer.emit(id);
     this.modal().toggle();
+  }
+
+  handleDeleteClick(customer: ICustomer) {
+    this.customerToDelete = customer;
+    this.deleteModal().toggle();
+  }
+
+  handleConfirmDelete() {
+    if (this.customerToDelete) {
+      this.onDeleteCustomer.emit({
+        customerId: this.customerToDelete.customerId,
+        modal: this.deleteModal(),
+      });
+      this.customerToDelete = null;
+    }
+  }
+
+  handleCancelDelete() {
+    this.deleteModal().toggle();
+    this.customerToDelete = null;
   }
 }
