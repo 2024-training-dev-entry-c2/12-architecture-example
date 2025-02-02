7ce5faaa6a6139f0a5079d6be5862b68671a97c1
Johan <johanvilla64@gmail.com>
2025-02-02 15:45:08 -0500

    feat(menu-service): add CreateMenuService for handling menu creation via HTTP

---
+++ projects/menu/src/infrastructure/services/create-menu.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { RESOURCES } from 'shared';
+import { IMenu } from '../../domain/models/menu.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CreateMenuService {
+  private _http = inject(HttpClient);
+
+  execute(menu: IMenu) {
+    return this._http.post<IMenu>(RESOURCES.CUSTOMERS, menu);
+  }
+}
