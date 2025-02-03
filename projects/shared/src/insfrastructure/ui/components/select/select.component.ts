ea4d79fb80a34c7a0e7bfd314c6040c35bc79b63
Johan <johanvilla64@gmail.com>
2025-02-03 07:16:24 -0500

    fix(component): :bug: enhance select component with ControlValueAccessor implementation for better form integration

--- projects/shared/src/insfrastructure/ui/components/select/select.component.ts
+++ projects/shared/src/insfrastructure/ui/components/select/select.component.ts
@@ -1,27 +1,59 @@
-import { Component, input, output } from '@angular/core';
+import { Component, forwardRef, input, output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { SelectOption } from '../../../../domain/models/select-component.model';
 
 @Component({
   selector: 'lib-select',
   standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true,
+    },
+  ],
   templateUrl: './select.component.html',
-  styleUrl: './select.component.scss',
 })
-export class SelectComponent<T> {
+export class SelectComponent<T> implements ControlValueAccessor {
   public options = input<SelectOption<T>[]>([]);
   public placeholder = input<string>('Selecciona una opción');
   public selectionChange = output<T>();
 
   public isOpen = false;
   public selectedLabel = '';
-  public activeOptionId = '';
+  private value: T | null = null;
+  private onChange: (value: T | null) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  
+  writeValue(value: T): void {
+    this.value = value;
+    const option = this.options()?.find((opt) => opt.value === value);
+    this.selectedLabel = option?.label || '';
+  }
+
+  
+  registerOnChange(fn: (value: T | null) => void): void {
+    this.onChange = fn;
+  }
+
+  
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
 
   toggleDropdown(): void {
     this.isOpen = !this.isOpen;
+    if (!this.isOpen) {
+      this.onTouched();
+    }
   }
 
   onSelect(option: SelectOption<T>): void {
+    this.value = option.value;
     this.selectedLabel = option.label;
+    this.onChange(this.value); 
+    this.onTouched(); 
     this.selectionChange.emit(option.value);
     this.isOpen = false;
   }
