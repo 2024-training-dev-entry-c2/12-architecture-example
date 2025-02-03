69674c183ecc25aa9ef7991909d8ddec089ab855
Johan <johanvilla64@gmail.com>
2025-02-03 05:41:54 -0500

    feat(component): :sparkles: add ListOrdersComponent with modal and order table

---
+++ projects/order/src/infrastructure/ui/components/list-orders/list-orders.component.ts
@@ -0,0 +1,27 @@
+import { Component, viewChild, input, output } from '@angular/core';
+import { AsyncPipe, DatePipe, NgFor } from '@angular/common';
+import { ModalComponent, SelectOption } from 'shared';
+import { IOrder, IOrderForm } from '../../../../domain/models/order.model';
+import { OrderFormComponent } from '../../forms/order-form/order-form.component';
+
+@Component({
+  selector: 'lib-list-orders',
+  standalone: true,
+  imports: [ModalComponent, OrderFormComponent, NgFor, DatePipe],
+  templateUrl: './list-orders.component.html',
+  styleUrl: './list-orders.component.scss',
+})
+export class ListOrdersComponent {
+  public modal = viewChild<ModalComponent>('modal');
+  public orders = input.required<IOrder[]>();
+  public customerOptions = input.required<SelectOption<number>[]>();
+  public dishOptions = input.required<SelectOption<number>[]>();
+  public onCreateOrder = output<{
+    order: IOrderForm;
+    modal: ModalComponent;
+  }>();
+
+  handleSubmit(orderForm: IOrderForm) {
+    this.onCreateOrder.emit({ order: orderForm, modal: this.modal() });
+  }
+}
