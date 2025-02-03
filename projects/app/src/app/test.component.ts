9a371fd882dc917e663cf2944f31635517337320
Johan <johanvilla64@gmail.com>
2025-02-03 08:23:36 -0500

    solving merge conflicts

--- projects/app/src/app/test.component.ts
+++ projects/app/src/app/test.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from '../../../shared/src/insfrastructure/ui/components/modal/modal.component';
 
 @Component({
   selector: 'app-test',
-  imports: [],
-  template: `<h1>Hola</h1>`,
+  imports: [ModalComponent, ModalComponent],
+  template: `
+    <h1>Hola</h1>
+    <lib-modal [action]="'test'">
+      <p>esto es una prueba de la anidacion</p>
+    </lib-modal>
+  `,
 })
 export class TestComponent {}
