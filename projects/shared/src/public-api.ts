23880dae7ad4196672c403da8e7609f9142bfbfa
Johan <johanvilla64@gmail.com>
2025-02-03 04:48:16 -0500

    fix(public-api): :bug: export SelectOption type from select-component model for improved type safety an d external lib utilization

--- projects/shared/src/public-api.ts
+++ projects/shared/src/public-api.ts
@@ -2,3 +2,4 @@ export * from './environments/environment';
 export { ModalComponent } from './insfrastructure/ui/components/modal/modal.component';
 export { SelectComponent } from './insfrastructure/ui/components/select/select.component';
 export { MainLayoutComponent } from './insfrastructure/ui/layouts/main-layout/main-layout.component';
+export type { SelectOption } from './domain/models/select-component.model';
