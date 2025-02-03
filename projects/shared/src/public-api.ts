61375b0c708ea734f395f132c76a942874115c21
Johan <johanvilla64@gmail.com>
2025-02-03 04:27:29 -0500

    feat(public-api): :sparkles: export SelectComponent from shared lib

--- projects/shared/src/public-api.ts
+++ projects/shared/src/public-api.ts
@@ -1,3 +1,4 @@
-export { ModalComponent } from './insfrastructure/ui/components/modal/modal.component';
-export { MainLayoutComponent } from './insfrastructure/ui/layouts/main-layout/main-layout.component';
 export * from './environments/environment';
+export { ModalComponent } from './insfrastructure/ui/components/modal/modal.component';
+export { SelectComponent } from './insfrastructure/ui/components/select/select.component';
+export { MainLayoutComponent } from './insfrastructure/ui/layouts/main-layout/main-layout.component';
