46b996797ff6c7de40393ed20720e0179057e9a4
Johan <johanvilla64@gmail.com>
2025-02-07 07:42:38 -0500

    feat(public-api): export SelectOption type and ButtonComponent for improved accessibility and test forms

--- projects/shared/src/public-api.ts
+++ projects/shared/src/public-api.ts
@@ -1,5 +1,6 @@
+export type { SelectOption } from './domain/models/select-component.model';
 export * from './environments/environment';
+export { ButtonComponent } from './insfrastructure/ui/components/button/button.component';
 export { ModalComponent } from './insfrastructure/ui/components/modal/modal.component';
 export { SelectComponent } from './insfrastructure/ui/components/select/select.component';
 export { MainLayoutComponent } from './insfrastructure/ui/layouts/main-layout/main-layout.component';
-export type { SelectOption } from './domain/models/select-component.model';
